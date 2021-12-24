3e57e256c3a1c265d24c8f924e4658525fe7ae5f
Imran Rather <emmeiwhite@gmail.com>
2021-12-25 02:54:06 +0530

    globals in nodejs

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const num = 12;
+
+if (num < 20) {
+  console.log(`number : ${num} is less is 20`);
+} else {
+  console.log(`number : ${num} is greater than 20`);
+}
+
+console.log("Running JS outside Browser");
