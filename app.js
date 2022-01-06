482bb0cc024b2affd5c580ec832c2d225e29f2ec
Imran Rather <emmeiwhite@gmail.com>
2022-01-07 03:16:40 +0530

    Introducing Modules

---
+++ app.js
@@ -0,0 +1,12 @@
+// Modules
+
+const JOHN = "John";
+const PETER = "Peter";
+
+const sayHi = (userName) => {
+  console.log(`Hello there :${userName}`);
+};
+
+sayHi("Emmei");
+sayHi(PETER);
+sayHi(JOHN);
