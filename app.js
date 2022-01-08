ddca124802dc46294a13d1713dcfc1710564e5a2
Imran Rather <emmeiwhite@gmail.com>
2022-01-09 00:56:58 +0530

    {flag: 'a'} to append to the file while writing

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //  fs module: 1) sync vs 2 async
 
 const {readFileSync,writeFileSync} = require('fs');
-const { readFile } = require('fs/promises');
 
 const first = readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
@@ -12,4 +11,5 @@ console.log(first, second);
 writeFileSync(
     './content/result-sync.txt',
     `Here is the result: ${first}, ${second}`,
-)
+    {flag:'a'}
+);
